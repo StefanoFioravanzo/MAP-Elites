4ed6f5ff0c0e57ccc8af9af9571cefcc9746b516
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-01-08 15:11:35 +0100

    Added exit code

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -28,6 +28,7 @@ functions=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14"
 if [[ -z ${LOGPATH+x} ]]
 then
     echo Log path not set
+    exit 1
 else
     echo Logging job to ${LOGPATH}
 fi
