12acb7478054d988c7f013efa117b43286fdf88d
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2019-01-08 15:10:21 +0100

    Added logpath option to launch experiments script

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -9,6 +9,10 @@ do
         -d|--delete)
         DELETE_SESSIONS=True
         shift # past argument
+        ;;
+        -l|--logpath)
+        LOGPATH="$2"
+        shift # past argument
         shift # past value
         ;;
         *)    # unknown option
@@ -20,6 +24,13 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 functions=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18")
+
+if [[ -z ${LOGPATH+x} ]]
+then
+    echo Log path not set
+else
+    echo Logging job to ${LOGPATH}
+fi
 
 for i in ${functions[*]}
 do
